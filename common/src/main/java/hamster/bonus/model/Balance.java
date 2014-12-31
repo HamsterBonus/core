38a1e883a1014bc1a529147e08a718843d633ae6
hamster1980 <79819721701@yandex,ru>
2014-12-31 14:10:48 +0400

    user and operation models entities

--- common/src/main/java/hamster/bonus/model/Balance.java
+++ common/src/main/java/hamster/bonus/model/Balance.java
@@ -1,32 +1,18 @@
 package hamster.bonus.model;
 
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.data.domain.Persistable;
-
 import com.google.common.base.Objects;
 
-public class Balance implements Persistable<String>{
+public class Balance extends AEntity {
 
 	private static final long serialVersionUID = 1L;
 
-	private final String id;
 	private final Amount activeValue;
 	private final Amount holdValue;
 	
 	public Balance(String id, Amount activeValue, Amount holdValue) {
-		this.id = id;
+		super(id);
 		this.activeValue = activeValue;
 		this.holdValue = holdValue;
-	}
-
-	@Override
-	public String getId() {
-		return id;
-	}
-	
-	@Override
-	public boolean isNew() {
-		return StringUtils.isEmpty(id);
 	}
 
 	public Amount getActiveValue() {
@@ -38,23 +24,9 @@ public class Balance implements Persistable<String>{
 	}
 	
     @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (!(obj instanceof Balance)) {
-            return false;
-        }
-        Balance other = (Balance) obj;
-        return Objects.equal(id, other.id);
-    }
-
-    @Override
     public String toString() {
         return Objects.toStringHelper(this)
-                        .add("id", id)
+                        .add("id", getId())
                         .add("activeValue", activeValue)
                         .add("holdValue", holdValue)
                             .toString();
--- common/src/main/java/hamster/bonus/model/Transaction.java
+++ common/src/main/java/hamster/bonus/model/Transaction.java
@@ -1,35 +1,21 @@
 package hamster.bonus.model;
 
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.data.domain.Persistable;
-
 import com.google.common.base.Objects;
 import com.google.common.base.Preconditions;
 
-public class Transaction implements Persistable<String>{
+public class Transaction extends AEntity {
 
 	private static final long serialVersionUID = 1L;
 
-	private final String id;
 	private final String sourceBalance;
 	private final String destinationBalance;
 	private final Amount amount;
 	
 	public Transaction(String id, String sourceBalance, String destinationBalance, Amount amount) {
-		this.id = id;
+		super(id);
 		this.sourceBalance = Preconditions.checkNotNull(sourceBalance);
 		this.destinationBalance = Preconditions.checkNotNull(destinationBalance);
 		this.amount = Preconditions.checkNotNull(amount);
-	}
-
-	@Override
-	public String getId() {
-		return id;
-	}
-
-	@Override
-	public boolean isNew() {
-		return StringUtils.isEmpty(id);
 	}
 
 	public String getSourceBalance() {
@@ -45,23 +31,9 @@ public class Transaction implements Persistable<String>{
 	}
 
     @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (!(obj instanceof Transaction)) {
-            return false;
-        }
-        Transaction other = (Transaction) obj;
-        return Objects.equal(id, other.id);
-    }
-
-    @Override
     public String toString() {
         return Objects.toStringHelper(this)
-                        .add("id", id)
+                        .add("id", getId())
                         .add("sourceBalance", sourceBalance)
                         .add("destinationBalance", destinationBalance)
                         .add("amount", amount)
